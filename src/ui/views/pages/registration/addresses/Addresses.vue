1e9052f2411acce6538476b70dde50a2abafdcf2
Leo Clipet <lclipet@redcross.ca>
2021-04-29 16:13:37 +0000

    Merged PR 2939: FIX - i18n
    FIX - i18n
    
    https://dev.azure.com/CRC-Tech/EMIS/_git/registration-lib/pullrequest/2938

--- src/ui/views/pages/registration/addresses/Addresses.vue
+++ src/ui/views/pages/registration/addresses/Addresses.vue
@@ -1,15 +1,21 @@
 <template>
-  <lib-addresses />
+  <lib-addresses :i18n="i18n" />
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import { Addresses as LibAddresses } from '@crctech/registration-lib';
+import { i18n } from '@/ui/plugins';
 
 export default Vue.extend({
   name: 'Addresses',
   components: {
     LibAddresses,
+  },
+  data() {
+    return {
+      i18n,
+    };
   },
 });
 </script>
--- src/ui/views/pages/registration/household-members/HouseholdMembers.vue
+++ src/ui/views/pages/registration/household-members/HouseholdMembers.vue
@@ -1,17 +1,24 @@
 <template>
-  <lib-household-members />
+  <lib-household-members :i18n="i18n" />
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 
 import { HouseholdMembers as LibHouseholdMembers } from '@crctech/registration-lib';
+import { i18n } from '@/ui/plugins';
 
 export default Vue.extend({
   name: 'HouseholdMembers',
 
   components: {
     LibHouseholdMembers,
+  },
+
+  data() {
+    return {
+      i18n,
+    };
   },
 });
 </script>
