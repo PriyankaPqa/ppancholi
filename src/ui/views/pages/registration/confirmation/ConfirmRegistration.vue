3de7a498e83f89da6ae3a491af594864efa5b317
Meng Xue <mxue@crctechmain.onmicrosoft.com>
2021-07-07 18:44:43 +0000

    Merged PR 3937: EMISV2-242: CRC Registration - Confirmation of Registration Screen
    story: https://rctech.atlassian.net/browse/EMISV2-242

--- src/ui/views/pages/registration/confirmation/ConfirmRegistration.vue
+++ src/ui/views/pages/registration/confirmation/ConfirmRegistration.vue
@@ -1,123 +1,21 @@
 <template>
-  <div class="fixed-height pb-12">
-    <template v-if="success">
-      <div class="fixed-height mb-n8">
-        <v-row no-gutters>
-          <v-col cols="12" class="rc-body16 mb-3" data-test="confirm-registration-message">
-            {{ confirmationMessage }}
-          </v-col>
-
-          <v-col cols="12" sm="6" class="grey-container pt-6 pl-6">
-            <span class="rc-body12">{{ $t('event.beneficiaries.registration_number') }}</span>
-            <br>
-            <span class="registration-result" data-test="confirm-registration-number">{{ registrationNumber }}</span>
-          </v-col>
-          <v-col cols="12" sm="6" class="grey-container pa-6">
-            <span class="rc-body12">{{ $t('registration.confirmation.event') }}</span>
-            <br>
-            <span class="registration-result" data-test="confirm-registration-event-name">{{ $m(event.name) }}</span>
-          </v-col>
-
-          <v-col cols="12" class="rc-body14 mt-3">
-            <i18n path="registration.confirmation.additional_assistance" tag="div">
-              <template #phone>
-                <span class="fw-bold" data-test="confirm-registration-phoneAssistance">{{ phoneAssistance }}</span>
-              </template>
-            </i18n>
-          </v-col>
-        </v-row>
-      </div>
-
-      <!-- TODO -->
-      <!-- <v-icon>mdi-information</v-icon>
-      <span class="rc-body14 ml-2">{{ $t('registration.confirmation.email_sent') }}</span> -->
-    </template>
-    <div v-else>
-      <confirmation-error :errors="errors" :phone="phoneAssistance" />
-    </div>
-  </div>
+  <lib-confirm-registration :i18n="i18n" />
 </template>
 
 <script lang="ts">
-import { IHouseholdEntity } from '@crctech/registration-lib/src/entities/household';
-import { IEventData } from '@crctech/registration-lib/src/entities/event';
-import { TranslateResult } from 'vue-i18n';
 import Vue from 'vue';
-import { IError } from '@/services/httpClient';
-import ConfirmationError from '@/ui/views/pages/registration/confirmation/ConfirmationError.vue';
-import { IRegistrationMenuItem } from '@crctech/registration-lib/src/types';
-import { IHouseholdCreate } from '@crctech/registration-lib/src/entities/household-create';
+import { ConfirmRegistration as LibConfirmRegistration } from '@crctech/registration-lib';
+import { i18n } from '@/ui/plugins';
 
 export default Vue.extend({
   name: 'ConfirmRegistration',
-  components: { ConfirmationError },
-  computed: {
-    success(): boolean {
-      return this.errors?.length === 0;
-    },
-
-    response(): IHouseholdEntity {
-      return this.$storage.registration.getters.registrationResponse();
-    },
-
-    household(): IHouseholdCreate {
-      return this.$storage.registration.getters.householdCreate();
-    },
-
-    errors(): IError[] {
-      return this.$storage.registration.getters.registrationErrors();
-    },
-
-    event(): IEventData {
-      return this.$storage.registration.getters.event();
-    },
-
-    confirmationMessage(): TranslateResult {
-      if (this.response) {
-        const identity = this.household.primaryBeneficiary.identitySet;
-        const fullName = `${identity.firstName} ${identity.middleName} ${identity.lastName}`;
-        return this.$t('registration.confirmation.thank_you', { x: fullName });
-      }
-      return '';
-    },
-
-    registrationNumber(): string {
-      return this.response?.registrationNumber;
-    },
-
-    phoneAssistance(): string {
-      return this.event.responseDetails?.assistanceNumber;
-    },
+  components: {
+    LibConfirmRegistration,
   },
-
-  watch: {
-    success: {
-      immediate: true,
-      handler(success) {
-        if (!success) {
-          this.setTabWithError();
-        }
-      },
-    },
-  },
-  methods: {
-    setTabWithError() {
-      this.$storage.registration.mutations.mutateCurrentTab((tab: IRegistrationMenuItem) => {
-        tab.nextButtonTextKey = 'common.cancel';
-        tab.titleKey = 'registration.confirmation.error';
-        tab.isValid = false;
-      });
-    },
+  data() {
+    return {
+      i18n,
+    };
   },
 });
 </script>
-
-<style scoped lang="scss">
-.fixed-height {
-  height: 100%;
-}
-.registration-result {
-  font-size: 24px;
-  font-weight: 500;
-}
-</style>
--- src/ui/views/pages/registration/confirmation/ConfirmationPrint.vue
+++ src/ui/views/pages/registration/confirmation/ConfirmationPrint.vue
@@ -1,50 +1,15 @@
 <template>
-  <div class="page-container">
-    <img v-if="$i18n.locale === 'en'" src="../../../../../../public/img/logos/rc/rc-en.svg" class="logo" alt="logo">
-    <img v-else src="../../../../../../public/img/logos/rc/rc-fr.svg" class="logo" alt="logo">
-    <div class="rc-body14 pt-8">
-      {{ $t('registration.type.individual') }}
-    </div>
-    <div class="rc-heading-4 pt-4">
-      {{ $t('registration.page.confirmation') }}
-    </div>
-    <v-divider class="py-4 mt-4" />
-    <div class="line">
-      <div class="rc-body14 ">
-        {{ $t('registration.confirmation.registration_id') }}: <span class="fw-bold">{{ $route.query.confirmationID }}</span>
-      </div>
-      <div class="rc-body14 pl-8">
-        {{ $t('registration.confirmation.event') }} <span class="fw-bold">{{ $route.query.eventName }}</span>
-      </div>
-    </div>
-    <div class="rc-body14 text-center footer">
-      {{ $t('registration.confirmation.additional_assistance', {phone: $route.query.phoneAssistance}) }}
-    </div>
-  </div>
+  <lib-confirmation-print />
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import { ConfirmationPrint as LibConfirmationPrint } from '@crctech/registration-lib';
 
 export default Vue.extend({
-  name: 'Confirmation',
+  name: 'ConfirmationPrint',
+  components: {
+    LibConfirmationPrint,
+  },
 });
 </script>
-
-<style lang="scss">
-  .line {
-    display:flex;
-  }
-  .footer {
-    position: absolute;
-    bottom: 0px;
-  }
-  .page-container {
-    padding: 40px;
-    position: relative;
-    height: 100%;
-  }
-.logo {
-  max-width: 165px;
-}
-</style>
