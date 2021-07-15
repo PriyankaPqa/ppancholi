e39a1451a24b20d3ccd2d0847aa79e328a89c6bb
Léo Clipet <lclipet@redcross.ca>
2021-07-15 13:03:32 +0000

    Merged PR 4000: Duplicate CRC
    https://rctech.atlassian.net/browse/EMISV2-245

---
+++ src/ui/views/pages/registration/confirmation/ConfirmRegistration.spec.js
@@ -0,0 +1,51 @@
+import { createLocalVue, shallowMount } from '@/test/testSetup';
+
+import { mockStorage } from '@/store/storage';
+import { tabs } from '@/store/modules/registration/tabs';
+import Component from './ConfirmRegistration.vue';
+
+const localVue = createLocalVue();
+const storage = mockStorage();
+
+describe('ConfirmRegistration.vue', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    wrapper = shallowMount(Component, {
+      localVue,
+      mocks: {
+        $storage: storage,
+      },
+    });
+  });
+
+  describe('Methods', () => {
+    describe('goToSearch', () => {
+      it('should call resetAllTabs', () => {
+        wrapper.vm.resetAllTabs = jest.fn();
+        wrapper.vm.goToSearch();
+        expect(wrapper.vm.resetAllTabs).toHaveBeenCalledTimes(1);
+      });
+
+      it('should call redirect to first page', () => {
+        wrapper.vm.resetAllTabs = jest.fn();
+        wrapper.vm.goToSearch();
+        expect(wrapper.vm.$storage.registration.mutations.setCurrentTabIndex).toHaveBeenCalledWith(tabs().findIndex((t) => t.id === 'isRegistered'));
+      });
+
+      it('should reset household create', () => {
+        wrapper.vm.resetAllTabs = jest.fn();
+        wrapper.vm.goToSearch();
+        expect(wrapper.vm.$storage.registration.mutations.resetHouseholdCreate).toHaveBeenCalledTimes(1);
+      });
+
+      it('should reset registration errors', () => {
+        wrapper.vm.resetAllTabs = jest.fn();
+        wrapper.vm.goToSearch();
+        expect(wrapper.vm.$storage.registration.mutations.setRegistrationErrors).toHaveBeenCalledTimes(1);
+        expect(wrapper.vm.$storage.registration.mutations.setRegistrationErrors).toHaveBeenCalledWith([]);
+      });
+    });
+  });
+});
--- src/ui/views/pages/registration/confirmation/ConfirmRegistration.vue
+++ src/ui/views/pages/registration/confirmation/ConfirmRegistration.vue
@@ -1,11 +1,13 @@
 <template>
-  <lib-confirm-registration :i18n="i18n" />
+  <lib-confirm-registration :i18n="i18n" @search-household="goToSearch()" />
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import { ConfirmRegistration as LibConfirmRegistration } from '@crctech/registration-lib';
 import { i18n } from '@/ui/plugins';
+import { tabs } from '@/store/modules/registration/tabs';
+import { IRegistrationMenuItem } from '@crctech/registration-lib/src/types';
 
 export default Vue.extend({
   name: 'ConfirmRegistration',
@@ -16,6 +18,30 @@ export default Vue.extend({
     return {
       i18n,
     };
+  },
+
+  computed: {
+    allTabs(): IRegistrationMenuItem[] {
+      return this.$storage.registration.getters.tabs();
+    },
+  },
+  methods: {
+    goToSearch() {
+      this.resetAllTabs();
+      this.$storage.registration.mutations.setCurrentTabIndex(tabs().findIndex((t) => t.id === 'isRegistered'));
+      this.$storage.registration.mutations.resetHouseholdCreate();
+      this.$storage.registration.mutations.setRegistrationErrors([]);
+    },
+
+    resetAllTabs() {
+      for (let index = 0; index < this.allTabs.length; index += 1) {
+        this.$storage.registration.mutations.mutateTabAtIndex(index, (tab: IRegistrationMenuItem) => {
+          tab.disabled = false;
+          tab.isValid = true;
+          tab.isTouched = false;
+        });
+      }
+    },
   },
 });
 </script>
