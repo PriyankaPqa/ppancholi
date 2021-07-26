1c028339520b5d7a3f9c7c476942c9b4c288f95b
Charlie Laplante <claplante@crctechmain.onmicrosoft.com>
2021-07-26 13:55:13 +0000

    Merged PR 4265: Case file signal R
    Update signalR.ts

--- src/ui/plugins/signalR.ts
+++ src/ui/plugins/signalR.ts
@@ -1,8 +1,8 @@
 /* eslint-disable */
-import AuthenticationProvider from "@/auth/AuthenticationProvider";
-import { IRootState } from "@/store";
-import { HubConnection, HubConnectionBuilder } from "@microsoft/signalr";
-import { Store } from "vuex";
+import AuthenticationProvider from '@/auth/AuthenticationProvider';
+import { IRootState } from '@/store';
+import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
+import { Store } from 'vuex';
 
 export class SignalRConnection {
   private connection: HubConnection;
@@ -25,7 +25,7 @@ export class SignalRConnection {
           accessTokenFactory: async () => {
             const tokenResponse = await AuthenticationProvider.acquireToken();
             return tokenResponse.accessToken;
-          }
+          },
         })
         // https://docs.microsoft.com/en-us/aspnet/core/signalr/javascript-client?view=aspnetcore-3.1#reconnect-clients
         .withAutomaticReconnect()
@@ -39,18 +39,46 @@ export class SignalRConnection {
     }
   }
 
-  private createBindings() {    
+  private createBindings() {
+    // Event
     this.listenForEntityChanges('event', 'AgreementType');
     this.listenForEntityChanges('event', 'Event');
-    this.listenForEntityChanges('event', 'EventMetadata');
+    this.listenForMetadataChanges('event', 'EventMetadata');
     this.listenForEntityChanges('event', 'EventType');
     this.listenForEntityChanges('event', 'Program');
-    this.listenForEntityChanges('event', 'ProgramMetadata');
+    this.listenForMetadataChanges('event', 'ProgramMetadata');
+
+    //CaseFile
+    this.listenForEntityChanges('caseFile', 'CaseFile');
+    this.listenForMetadataChanges('caseFile', 'CaseFileMetadata');
+    this.listenForEntityChanges('caseFile', 'CaseNote');
+    this.listenForMetadataChanges('caseFile', 'CaseNoteMetadata');
+    this.listenForEntityChanges('caseFile', 'Referral');
+    this.listenForMetadataChanges('caseFile', 'ReferralMetadata');
   }
 
   private listenForEntityChanges(domain: string, entityName: string) {
-    this.connection.on(`${domain}.${entityName}Created`, data => { console.log(`${domain}.${entityName}Created`, data) });
-    this.connection.on(`${domain}.${entityName}Updated`, data => { console.log(`${domain}.${entityName}Updated`, data) });    
+    this.connection.on(`${domain}.${entityName}Updated`, (entity) => {
+      console.log(`${domain}.${entityName}Updated`, entity);
+      this.store.commit(`${domain}Entities/set`, entity);
+    });
+
+    this.connection.on(`${domain}.${entityName}Created`, (entity) => {
+      console.log(`${domain}.${entityName}Created`, entity);
+      this.store.commit(`${domain}Entities/set`, entity);
+    });
+  }
+
+  private listenForMetadataChanges(domain: string, entityName: string) {
+    this.connection.on(`${domain}.${entityName}Updated`, (entity) => {
+      console.log(`${domain}.${entityName}Updated`, entity);
+      this.store.commit(`${domain}Metadata/set`, entity);
+    });
+
+    this.connection.on(`${domain}.${entityName}Created`, (entity) => {
+      console.log(`${domain}.${entityName}Created`, entity);
+      this.store.commit(`${domain}Metadata/set`, entity);
+    });
   }
 }
 
