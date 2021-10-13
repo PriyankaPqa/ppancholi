40e180e10eb73db5947bf7b10899b7b738df2fdd
Marc-André Deschênes <MaDeschenes@crctechmain.onmicrosoft.com>
2021-10-13 18:11:36 +0000

    Merged PR 5334: 2250

--- src/store/store.types.ts
+++ src/store/store.types.ts
@@ -1,5 +1,4 @@
 import { Store } from 'vuex';
-import { IProvider, IProviderMock } from '@/services/provider';
 import { IState as IRegistrationState } from './modules/registration/registration.types';
 import { IHouseholdEntityState } from './modules/household/householdEntity.types';
 
@@ -13,5 +12,5 @@ export type IState = IRootState;
 
 export interface IStore<S> extends Store<S> {
   // eslint-disable-next-line
-  $services: IProvider | IProviderMock;
+  $services: any;
 }
