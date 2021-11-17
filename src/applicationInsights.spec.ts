c04e90ba3d14762572e352f27fd8f3bd15e6e083
Éric-Karl Matteau <EMatteau@crctechmain.onmicrosoft.com>
2021-11-17 16:05:10 +0000

    Merged PR 5856: Adjusted App Insights tests
    Adjusted App Insights tests

--- src/applicationInsights.spec.ts
+++ src/applicationInsights.spec.ts
@@ -1,4 +1,4 @@
-// /* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { ApplicationInsights, Util } from '@microsoft/applicationinsights-web';
 import { NavigationGuard, NavigationGuardNext } from 'vue-router';
 import applicationInsights from './applicationInsights';
@@ -54,52 +54,65 @@ describe('ApplicationInsights', () => {
     });
   });
 
-  // it('should not throw load app insights fails', () => {
-  //   appInsights.loadAppInsights.mockImplementation(() => {
-  //     throw new Error('Failed to load');
-  //   });
-  //
-  //   expect(() => install(vue, { connectionString: '', router, appName })).not.toThrow();
-  // });
-  //
-  // describe('Once properly initialized', () => {
-  //   beforeEach(() => {
-  //     install(vue, { connectionString, router, appName });
-  //   });
-  //
-  //   it('should generate new traceID when routing to new page', () => {
-  //     const traceIDBeforeRouting = appInsights.context.telemetryTrace.traceID;
-  //
-  //     navigateTo('new.route', 'current.route', jest.fn());
-  //
-  //     const traceIDAfterRouting = appInsights.context.telemetryTrace.traceID;
-  //     expect(Util.generateW3CId).toHaveBeenCalled();
-  //     expect(traceIDAfterRouting).toEqual('some new trace ID');
-  //     expect(traceIDBeforeRouting).not.toEqual(traceIDAfterRouting);
-  //   });
-  //
-  //   it('should track new page view when routing to new page', () => {
-  //     navigateTo('new.route', 'current.route', jest.fn());
-  //
-  //     expect(appInsights.trackPageView).toHaveBeenCalledWith({
-  //       name: 'TestEMIS - new.route',
-  //     });
-  //   });
-  //
-  //   it('should continue the middleware chain on the beforeEach navigation guard', () => {
-  //     const next = jest.fn();
-  //
-  //     navigateTo('new.route', 'current.route', next);
-  //
-  //     expect(next).toHaveBeenCalled();
-  //   });
-  //
-  //   it('should intercept error and send them to application insights, then rethrow', () => {
-  //     const error = new Error('Whoopsie!');
-  //
-  //     expect(() => vue.config.errorHandler(error)).toThrow('Whoopsie!');
-  //
-  //     expect(appInsights.trackException).toHaveBeenCalledWith({ exception: error, properties: {} });
-  //   });
-  // });
+  it('should not throw load app insights fails during prepare', () => {
+    appInsights.loadAppInsights.mockImplementation(() => {
+      throw new Error('Failed to load');
+    });
+
+    expect(() => applicationInsights.initialize({ connectionString: '', router, appName })).not.toThrow();
+  });
+
+  it('should not throw load app insights fails during vue install', () => {
+    const failingRouter:any = { beforeEach: jest.fn() };
+    failingRouter.beforeEach.mockImplementation(() => {
+      throw new Error('Failed to install');
+    });
+    const options = { connectionString: '', router: failingRouter, appName };
+    applicationInsights.initialize(options);
+
+    expect(() => applicationInsights.install(vue, options)).not.toThrow();
+  });
+
+  describe('Once properly initialized', () => {
+    beforeEach(() => {
+      const options = { connectionString, router, appName };
+      applicationInsights.initialize(options);
+      applicationInsights.install(vue, options);
+    });
+
+    it('should generate new traceID when routing to new page', () => {
+      const traceIDBeforeRouting = appInsights.context.telemetryTrace.traceID;
+
+      navigateTo('new.route', 'current.route', jest.fn());
+
+      const traceIDAfterRouting = appInsights.context.telemetryTrace.traceID;
+      expect(Util.generateW3CId).toHaveBeenCalled();
+      expect(traceIDAfterRouting).toEqual('some new trace ID');
+      expect(traceIDBeforeRouting).not.toEqual(traceIDAfterRouting);
+    });
+
+    it('should track new page view when routing to new page', () => {
+      navigateTo('new.route', 'current.route', jest.fn());
+
+      expect(appInsights.trackPageView).toHaveBeenCalledWith({
+        name: 'TestEMIS - new.route',
+      });
+    });
+
+    it('should continue the middleware chain on the beforeEach navigation guard', () => {
+      const next = jest.fn();
+
+      navigateTo('new.route', 'current.route', next);
+
+      expect(next).toHaveBeenCalled();
+    });
+
+    it('should intercept error and send them to application insights, then rethrow', () => {
+      const error = new Error('Whoopsie!');
+
+      expect(() => vue.config.errorHandler(error)).toThrow('Whoopsie!');
+
+      expect(appInsights.trackException).toHaveBeenCalledWith({ exception: error, properties: {} });
+    });
+  });
 });
--- src/applicationInsights.ts
+++ src/applicationInsights.ts
@@ -16,57 +16,58 @@ class AppInsights {
   }
 
   public initialize(options: ApplicationInsightsOptions) {
-    this.instance = new ApplicationInsights({
-      config: {
-        connectionString: options.connectionString,
-        // If true, Fetch requests are not autocollected. This is verbose yet very useful in debugging issues.
-        disableFetchTracking: false,
-        // Enable Request-ID and Request-Context headers needed to correlate API calls
-        enableCorsCorrelation: true,
-        // If true, AJAX & Fetch request headers is tracked. Required for proper correlation.
-        enableRequestHeaderTracking: true,
-        // If true, AJAX & Fetch request's response headers is tracked. Required for proper correlation.
-        enableResponseHeaderTracking: true,
-      },
-    });
-    this.instance.loadAppInsights();
-
-    // This is needed to track the initial page load.
-    this.instance.trackPageView({
-      name: `${options.appName} - Initial Page Load`,
-    });
-
-    Vue.use(this, options);
-  }
-
-  public install(vue: typeof Vue, options: ApplicationInsightsOptions) {
     try {
-      this.performInstall(vue, options);
+      this.instance = new ApplicationInsights({
+        config: {
+          connectionString: options.connectionString,
+          // If true, Fetch requests are not autocollected. This is verbose yet very useful in debugging issues.
+          disableFetchTracking: false,
+          // Enable Request-ID and Request-Context headers needed to correlate API calls
+          enableCorsCorrelation: true,
+          // If true, AJAX & Fetch request headers is tracked. Required for proper correlation.
+          enableRequestHeaderTracking: true,
+          // If true, AJAX & Fetch request's response headers is tracked. Required for proper correlation.
+          enableResponseHeaderTracking: true,
+        },
+      });
+      this.instance.loadAppInsights();
+
+      // This is needed to track the initial page load.
+      this.instance.trackPageView({
+        name: `${options.appName} - Initial Page Load`,
+      });
+
+      Vue.use(this, options);
     } catch (error) {
       /* eslint-disable no-console */
       console.error(error);
     }
   }
 
-  private performInstall(vue: typeof Vue, options: ApplicationInsightsOptions) {
-    const { appName, router } = options;
+  public install(vue: typeof Vue, options: ApplicationInsightsOptions) {
+    try {
+      const { appName, router } = options;
 
-    // We intercept the error from vue to send it to application insights; then rethrow normally.
-    vue.config.errorHandler = (err) => {
-      this.instance.trackException({ exception: err, properties: {} });
-      throw err;
-    };
+      // We intercept the error from vue to send it to application insights; then rethrow normally.
+      vue.config.errorHandler = (err) => {
+        this.instance.trackException({ exception: err, properties: {} });
+        throw err;
+      };
 
-    // Register middleware to create new operation when changing page.
-    // https://router.vuejs.org/guide/advanced/navigation-guards.html#global-before-guards
-    router.beforeEach((to, _from, next) => {
-      this.instance.flush();
-      this.instance.context.telemetryTrace.traceID = Util.generateW3CId();
-      this.instance.trackPageView({
-        name: `${appName} - ${to.name}`,
+      // Register middleware to create new operation when changing page.
+      // https://router.vuejs.org/guide/advanced/navigation-guards.html#global-before-guards
+      router.beforeEach((to, _from, next) => {
+        this.instance.flush();
+        this.instance.context.telemetryTrace.traceID = Util.generateW3CId();
+        this.instance.trackPageView({
+          name: `${appName} - ${to.name}`,
+        });
+        next();
       });
-      next();
-    });
+    } catch (error) {
+      /* eslint-disable no-console */
+      console.error(error);
+    }
   }
 
   public trackException(error: Error, properties: Record<string, unknown> = {}) {
