460628f6300983f05255e00fccc68a6721b7ebab
Léo Clipet <lclipet@redcross.ca>
2021-11-29 13:50:45 +0000

    Merged PR 6021: 2871 - Remove accessibe
    https://rctech.atlassian.net/browse/EMISV2-2871

--- src/ui/App.vue
+++ src/ui/App.vue
@@ -27,42 +27,6 @@ export default {
       process.env.VUE_APP_GOOGLE_API_KEY,
     );
   },
-  mounted() {
-    const script = document.createElement('script');
-
-    script.src = 'https://acsbapp.com/apps/app/dist/js/app.js';
-    script.async = true;
-    script.onload = function accessiBe() {
-      // eslint-disable-next-line no-undef
-      acsbJS.init({
-        statementLink: '',
-        footerHtml: '',
-        hideMobile: false,
-        hideTrigger: false,
-        language: 'en',
-        position: 'right',
-        leadColor: '#146FF8',
-        triggerColor: '#146FF8',
-        triggerRadius: '50%',
-        triggerPositionX: 'right',
-        triggerPositionY: 'bottom',
-        triggerIcon: 'people',
-        triggerSize: 'medium',
-        triggerOffsetX: 20,
-        triggerOffsetY: 20,
-        mobile: {
-          triggerSize: 'small',
-          triggerPositionX: 'right',
-          triggerPositionY: 'center',
-          triggerOffsetX: 0,
-          triggerOffsetY: 0,
-          triggerRadius: '50%',
-        },
-      });
-    };
-
-    document.body.appendChild(script);
-  },
-
 };
+
 </script>
