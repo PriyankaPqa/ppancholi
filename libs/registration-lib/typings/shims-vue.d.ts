8c8b1064f2ada3f5c975a320cf32e008fe7086c1
Leo Clipet <55412660+leoclipet@users.noreply.github.com>
2022-02-28 22:00:26 -0500

    Add back shim files

---
+++ libs/registration-lib/typings/shims-vue.d.ts
@@ -0,0 +1,11 @@
+import Vue from 'vue';
+
+declare module '*.vue' {
+
+  export default Vue;
+
+  export type VForm = Vue & {
+    validate: (type?: { silent?: boolean }) => boolean;
+    reset: () => void;
+  }
+}
