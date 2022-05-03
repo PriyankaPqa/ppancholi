8aa5c903a0a609bac826b6f9d5d16542aca14634
Meng Xue <mxue@crctechmain.onmicrosoft.com>
2022-05-03 17:21:01 +0000

    Merged PR 7527: [EMISV2-4244] Role not updated in right menu
    https://rctech.atlassian.net/browse/EMISV2-4244
    
    feature branch: https://emis-4244.crc-tech.ca

--- apps/emis-app/src/ui/views/pages/system-management/lists/user-accounts/account-settings/AccountSettings.spec.js
+++ apps/emis-app/src/ui/views/pages/system-management/lists/user-accounts/account-settings/AccountSettings.spec.js
@@ -12,8 +12,6 @@ const mockUser = mockCombinedUserAccount();
 
 describe('AccountSettings.vue', () => {
   let wrapper;
-  let roleHasChanged = false;
-  let isLevel6 = false;
   storage.userAccount.getters.roles = jest.fn(() => mockOptionItemData());
   storage.userAccount.actions.fetchUserAccount = jest.fn(() => mockCombinedUserAccount());
   storage.userAccount.getters.get = jest.fn(() => mockCombinedUserAccount());
@@ -24,16 +22,11 @@ describe('AccountSettings.vue', () => {
       localVue,
       computed: {
         user: () => mockUser,
-        roleHasChanged: () => roleHasChanged,
         basicUserData: () => new User(mockUsersData()[0]),
       },
       mocks: {
-        $hasLevel: jest.fn((lv) => lv !== 'level5' || isLevel6),
         $storage: storage,
       },
-    });
-    await wrapper.setData({
-      loading: false,
     });
   };
 
@@ -144,63 +137,6 @@ describe('AccountSettings.vue', () => {
         expect(element.exists()).toBeTruthy();
       });
     });
-
-    describe('role dropdown', () => {
-      let element;
-
-      it('does not render when not level 6', async () => {
-        isLevel6 = false;
-        await doMount();
-
-        element = wrapper.findDataTest('user_roleId');
-        expect(element.exists()).toBeFalsy();
-      });
-
-      it('renders when level 6', async () => {
-        isLevel6 = true;
-        await doMount();
-
-        element = wrapper.findDataTest('user_roleId');
-        expect(element.exists()).toBeTruthy();
-      });
-    });
-
-    describe('role action buttons', () => {
-      let element;
-
-      it('renders when roleHasChanged = true', async () => {
-        roleHasChanged = true;
-        await doMount();
-
-        element = wrapper.findDataTest('apply-role-button');
-        expect(element.exists()).toBeTruthy();
-        element = wrapper.findDataTest('cancel-role-change');
-        expect(element.exists()).toBeTruthy();
-      });
-
-      it('does not render when roleHasChanged = false', async () => {
-        roleHasChanged = false;
-
-        await doMount();
-
-        element = wrapper.findDataTest('apply-role-button');
-        expect(element.exists()).toBeFalsy();
-        element = wrapper.findDataTest('cancel-role-change');
-        expect(element.exists()).toBeFalsy();
-      });
-    });
-  });
-
-  describe('life cycle', () => {
-    it('should call fetchUserAccount with id', async () => {
-      doMount();
-      wrapper.vm.fetchUserAccount = jest.fn();
-      wrapper.vm.setRoles = jest.fn();
-      await wrapper.vm.$options.created.forEach((hook) => {
-        hook.call(wrapper.vm);
-      });
-      expect(wrapper.vm.fetchUserAccount).toHaveBeenCalledWith(wrapper.vm.id);
-    });
   });
 
   describe('Computed', () => {
@@ -226,30 +162,9 @@ describe('AccountSettings.vue', () => {
       });
     });
 
-    describe('roles', () => {
-      it('returns the roles from the store ', async () => {
-        expect(wrapper.vm.roles).toEqual(mockOptionItemData());
-      });
-    });
-
     describe('user', () => {
       it('return the user account by id from the storage', () => {
         expect(wrapper.vm.user).toEqual(mockUser);
-      });
-    });
-
-    describe('roleHasChanged', () => {
-      it('returns true when current role is not equal to user role', async () => {
-        await wrapper.setData({
-          currentRole: { id: 'nope' },
-        });
-        expect(wrapper.vm.roleHasChanged).toBeTruthy();
-      });
-      it('returns false when current role is equal to user role', async () => {
-        await wrapper.setData({
-          currentRole: { id: mockUser.entity.roles[0].optionItemId },
-        });
-        expect(wrapper.vm.roleHasChanged).toBeFalsy();
       });
     });
 
@@ -303,191 +218,6 @@ describe('AccountSettings.vue', () => {
         });
 
         expect(wrapper.vm.preferredLanguage).toBe('account_settings.preferredLanguage.notSet');
-      });
-    });
-  });
-
-  describe('Methods', () => {
-    beforeEach(() => {
-      wrapper = shallowMount(Component, {
-        localVue,
-        store: {
-          userAccount: {
-            searchLoading: false,
-          },
-        },
-        computed: {
-          user() {
-            return mockUser;
-          },
-          roleHasChanged() {
-            return true;
-          },
-          id: () => 'myId',
-        },
-        mocks: {
-          $storage: storage,
-        },
-      });
-    });
-
-    describe('applyRoleChange', () => {
-      it('should be called when clicking apply', async () => {
-        const element = wrapper.findDataTest('apply-role-button');
-        wrapper.vm.applyRoleChange = jest.fn();
-        element.vm.$emit('click');
-        expect(wrapper.vm.applyRoleChange).toHaveBeenCalled();
-      });
-      it('applies a pending role change and triggers a success toast', async () => {
-        wrapper.vm.$toasted.global.success = jest.fn();
-        wrapper.vm.$storage.userAccount.actions.assignRole = jest.fn(() => mockUser);
-        const currentRole = { id: 'myId' };
-        await wrapper.setData({ currentRole });
-        await wrapper.vm.applyRoleChange();
-        wrapper.vm.$nextTick();
-
-        expect(wrapper.vm.loading).toBeFalsy();
-
-        expect(wrapper.vm.$storage.userAccount.actions.assignRole).toBeCalledWith({
-          subRole: currentRole,
-          userId: mockUser.entity.id,
-        });
-        expect(wrapper.vm.$toasted.global.success).toHaveBeenCalledTimes(1);
-      });
-    });
-
-    describe('resetCurrentRole', () => {
-      it('should be called when clicking cancel', async () => {
-        const element = wrapper.findDataTest('cancel-role-change');
-        wrapper.vm.resetCurrentRole = jest.fn();
-        element.vm.$emit('click');
-        expect(wrapper.vm.resetCurrentRole).toHaveBeenCalled();
-      });
-      it('resets currentRole to user role', async () => {
-        const mockSubItemData = [...mockOptionItemData()[0].subitems, mockOptionItemData()[1]];
-        const resettedOption = { id: mockUser.entity.roles[0].optionItemId, name: 'that is great' };
-        await wrapper.setData({ allRoles: mockSubItemData.concat(resettedOption) });
-        const currentRole = { id: 'myId', name: 'nope' };
-        await wrapper.setData({ currentRole });
-        wrapper.vm.resetCurrentRole();
-        wrapper.vm.$nextTick();
-
-        expect(wrapper.vm.currentRole).toEqual(resettedOption);
-      });
-    });
-
-    describe('setRoles', () => {
-      it('filters for active roles', async () => {
-        const itemData = mockOptionItemData();
-        itemData.forEach((a) => {
-          a.subitems.forEach((s) => {
-            s.status = 1;
-          });
-        });
-        itemData[0].subitems[0].status = 0;
-
-        await wrapper.vm.setRoles();
-        expect(wrapper.vm.allAccessLevelRoles).toContainEqual(itemData[0].subitems[1]);
-        expect(wrapper.vm.allAccessLevelRoles.filter((x) => x === itemData[0].subitems[0])[0]).toBeUndefined();
-      });
-    });
-
-    describe('setAllAccessLevelRoles', () => {
-      it('includes current user\'s inactive role', async () => {
-        const itemData = mockOptionItemData();
-        itemData.forEach((a) => {
-          a.subitems.forEach((s) => {
-            s.status = 1;
-          });
-        });
-        itemData[0].subitems[0].status = 0;
-
-        const user = mockUser;
-        user.entity.roles[0].optionItemId = itemData[0].subitems[0].id;
-
-        await wrapper.vm.setAllAccessLevelRoles(itemData);
-        expect(wrapper.vm.allAccessLevelRoles).toContain(itemData[0].subitems[1]);
-        expect(wrapper.vm.allAccessLevelRoles).toContain(itemData[0].subitems[0]);
-      });
-    });
-
-    describe('fetchUserAccount', () => {
-      storage.user.getters.userId = jest.fn(() => 'mock-id');
-      storage.userAccount.actions.fetch = jest.fn(() => {});
-
-      it('should call fetchUserAccount with the response of the user storage getter if none in route params', () => {
-        jest.clearAllMocks();
-        wrapper = shallowMount(Component, {
-          localVue,
-          mocks: {
-            $storage: storage,
-          },
-        });
-        expect(wrapper.vm.$storage.userAccount.actions.fetch).toHaveBeenCalledWith('mock-id');
-      });
-
-      it('should call fetchUserAccount with the route params', () => {
-        wrapper = shallowMount(Component, {
-          localVue,
-          mocks: {
-            $storage: storage,
-            $route: {
-              params: { id: 'abcd' },
-            },
-          },
-        });
-        expect(wrapper.vm.$storage.userAccount.actions.fetch).toHaveBeenCalledWith('abcd');
-        expect(wrapper.vm.id).toEqual('abcd');
-      });
-    });
-
-    describe('setNonEditableRolesForL5', () => {
-      it('returns user if user is in the notAllowed level', () => {
-        const level = {
-          subitems: [
-            {
-              id: 'id',
-            }, {
-              id: mockUser.entity.roles[0].optionItemId,
-            },
-          ],
-        };
-
-        const result = wrapper.vm.setNonEditableRolesForL5(level);
-        expect(result).toEqual([{
-          id: mockUser.entity.roles[0].optionItemId,
-        }]);
-      });
-
-      it('disable dropdown if user is in the notAllowed level', () => {
-        const level = {
-          subitems: [
-            {
-              id: 'id',
-            }, {
-              id: mockUser.entity.roles[0].optionItemId,
-            },
-          ],
-        };
-
-        expect(wrapper.vm.disableForL5).toEqual(false);
-        wrapper.vm.setNonEditableRolesForL5(level);
-        expect(wrapper.vm.disableForL5).toEqual(true);
-      });
-
-      it('returns empty array if user is not in the notAllowed level', () => {
-        const level = {
-          subitems: [
-            {
-              id: 'id1',
-            }, {
-              id: 'id2',
-            },
-          ],
-        };
-
-        const result = wrapper.vm.setNonEditableRolesForL5(level);
-        expect(result).toEqual([]);
       });
     });
   });
--- apps/emis-app/src/ui/views/pages/system-management/lists/user-accounts/account-settings/AccountSettings.vue
+++ apps/emis-app/src/ui/views/pages/system-management/lists/user-accounts/account-settings/AccountSettings.vue
@@ -1,7 +1,6 @@
 <template>
   <rc-page-content v-if="user" :title="$t('user.accountSettings.title')">
-    <rc-page-loading v-if="loading" data-test="loading" />
-    <v-row v-else class="justify-center">
+    <v-row class="justify-center">
       <v-col cols="12" lg="7">
         <v-sheet rounded outlined class="mt-8">
           <v-simple-table>
@@ -46,45 +45,8 @@
                 <td>
                   {{ $t('user.accountSettings.role') }}
                 </td>
-                <td class="fw-bold" data-test="userAccount-status-roleName" style="width: 40%">
-                  <div v-if="$hasLevel('level5')">
-                    <v-select-with-validation
-                      v-model="currentRole"
-                      :attach="false"
-                      item-value="id"
-                      :item-disabled="(item) => item.status !== activeStatus"
-                      :item-text="(item) => item ? $m(item.name) : ''"
-                      :disabled="disableForL5"
-                      dense
-                      outlined
-                      return-object
-                      hide-details
-                      data-test="user_roleId"
-                      :label="$t('system_management.userAccounts.role_header')"
-                      :items="allAccessLevelRoles" />
-                  </div>
-                  <div v-else>
-                    {{ user.metadata.roleName? $m(user.metadata.roleName) : basicUserData.roles[0] }}
-                  </div>
-                </td>
-                <td>
-                  <div v-if="roleHasChanged">
-                    <v-btn
-                      small
-                      color="primary"
-                      data-test="apply-role-button"
-                      @click="applyRoleChange()">
-                      {{ $t('common.apply') }}
-                    </v-btn>
-                    <v-btn
-                      icon
-                      data-test="cancel-role-change"
-                      @click="resetCurrentRole()">
-                      <v-icon>
-                        mdi-close
-                      </v-icon>
-                    </v-btn>
-                  </div>
+                <td colspan="2" class="fw-bold" data-test="userAccount-status-roleName">
+                  {{ user.metadata.roleName? $m(user.metadata.roleName) : basicUserData.roles[0] }}
                 </td>
               </tr>
             </tbody>
@@ -163,17 +125,10 @@
 import Vue from 'vue';
 import {
   RcPageContent,
-  RcPageLoading,
-  VSelectWithValidation,
   RcTooltip,
 } from '@libs/component-lib/components';
-import { NavigationGuardNext, Route } from 'vue-router';
 import { IUserAccountCombined, AccountStatus } from '@/entities/user-account';
 import StatusChip from '@/ui/shared-components/StatusChip.vue';
-import {
-  IOptionItem, IOptionSubItem,
-} from '@/entities/optionItem';
-import helpers from '@/ui/helpers/helpers';
 
 import { Status } from '@libs/core-lib/entities/base';
 
@@ -181,25 +136,13 @@ export default Vue.extend({
   name: 'AccountSettings',
   components: {
     RcPageContent,
-    RcPageLoading,
-    VSelectWithValidation,
     StatusChip,
     RcTooltip,
-  },
-
-  async beforeRouteLeave(to: Route, from: Route, next: NavigationGuardNext) {
-    await helpers.confirmBeforeLeaving(this, this.roleHasChanged, next);
   },
 
   data() {
     return {
       AccountStatus,
-      loading: false,
-      currentRole: null as IOptionSubItem,
-      allAccessLevelRoles: [] as Array<IOptionSubItem|{header: string, id?: string}>,
-      activeStatus: Status.Active,
-      allRoles: [] as IOptionSubItem[],
-      disableForL5: false,
       accountStatus: Status,
     };
   },
@@ -210,19 +153,8 @@ export default Vue.extend({
       return this.$storage.user.getters.user();
     },
 
-    roles(): IOptionItem[] {
-      return this.$storage.userAccount.getters.roles();
-    },
-
     user(): IUserAccountCombined {
       return this.$storage.userAccount.getters.get(this.id);
-    },
-
-    roleHasChanged() : boolean {
-      if (this.currentRole) {
-        return this.user?.entity?.roles && this.user.entity.roles[0]?.optionItemId !== this.currentRole?.id;
-      }
-      return false;
     },
 
     id(): string {
@@ -242,102 +174,6 @@ export default Vue.extend({
 
       return `${this.$t('account_settings.preferredLanguage.notSet')}`;
     },
-
-    isL5(): boolean {
-      return this.$hasLevel('level5') && !this.$hasLevel('level6');
-    },
-  },
-
-  async created() {
-    if (!this.$hasRole('noAccess')) {
-      await Promise.all([this.$storage.userAccount.actions.fetchRoles(), this.fetchUserAccount(this.id)]);
-      await this.setRoles();
-    }
-
-    this.resetCurrentRole();
-  },
-
-  methods: {
-    async setRoles() {
-      this.setAllAccessLevelRoles(this.roles);
-      this.allRoles = this.roles.reduce((acc: IOptionSubItem[], curr: IOptionItem) => acc.concat(curr.subitems), []);
-    },
-
-    // set the hierarchical list of roles and subroles in the format needed for the dropdown of the select component
-    setAllAccessLevelRoles(roles: IOptionItem[]) {
-      if (roles?.length) {
-        roles.forEach((accessLevel: IOptionItem) => {
-          let subitems: IOptionSubItem[];
-
-          if (this.isL5 && this.isLevelNotAllowedForL5(accessLevel)) {
-            subitems = this.setNonEditableRolesForL5(accessLevel);
-          } else {
-            subitems = accessLevel.subitems;
-          }
-
-          const activeSubRoles: IOptionSubItem[] = [];
-          subitems.forEach((role: IOptionSubItem) => {
-            if (role.status === Status.Active || (this.user.entity.roles && this.user.entity.roles[0]?.optionItemId === role.id)) {
-              activeSubRoles.push(role);
-            }
-          });
-          if (activeSubRoles.length) {
-            this.allAccessLevelRoles.push({
-              header: this.$m(accessLevel.name),
-            });
-            activeSubRoles.forEach((a) => this.allAccessLevelRoles.push(a));
-          }
-        });
-      }
-    },
-
-    async applyRoleChange() {
-      if (this.currentRole) {
-        try {
-          this.loading = true; // Visual cue of busy state
-          const request = {
-            subRole: this.currentRole,
-            userId: this.user.entity.id,
-          };
-          const r = await this.$storage.userAccount.actions.assignRole(request);
-          if (r) {
-            this.$toasted.global.success(this.$t('system_management.userAccounts.role_update_success'));
-          }
-        } finally {
-          this.loading = false;
-        }
-      }
-    },
-
-    resetCurrentRole() {
-      if (this.user.entity.roles && this.user.entity.roles[0]) {
-        this.currentRole = this.allRoles.find((l) => l.id === this.user.entity.roles[0].optionItemId) as IOptionSubItem;
-      }
-    },
-
-    async fetchUserAccount(id: string) {
-      this.loading = true;
-      try {
-        await this.$storage.userAccount.actions.fetch(id);
-      } finally {
-        this.loading = false;
-      }
-    },
-
-    isLevelNotAllowedForL5(level: IOptionItem): boolean {
-      return ['Level 6', 'ContributorIM', 'ContributorFinance', 'Contributor 3', 'Read Only']
-        .some((lev) => lev === level.name.translation.en);
-    },
-
-    setNonEditableRolesForL5(level: IOptionItem): IOptionSubItem[] {
-      const roleToDisable = level.subitems.find((sub) => sub.id === this.user.entity.roles[0]?.optionItemId);
-      if (roleToDisable) {
-        this.disableForL5 = true;
-        return [roleToDisable];
-      }
-      return [];
-    },
-
   },
 });
 </script>
