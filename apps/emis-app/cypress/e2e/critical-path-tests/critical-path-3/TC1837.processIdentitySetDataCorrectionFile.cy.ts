b73b4740652977803aa322f86ce323902410a20e
Manmohan Singh Brar <mbrar@crc-tech.ca>
2023-12-08 21:42:01 +0000

    Merged PR 11746: [EMISV2-7688] - TC1837 - Process an Identity Set data correction file
    https://rctech.atlassian.net/browse/EMISV2-7688

---
+++ apps/emis-app/cypress/e2e/critical-path-tests/critical-path-3/TC1837.processIdentitySetDataCorrectionFile.cy.ts
@@ -0,0 +1,98 @@
+import { MassActionDataCorrectionType } from '@libs/entities-lib/mass-action';
+import { UserRoles } from '@libs/cypress-lib/support/msal';
+import { getRoles } from '@libs/cypress-lib/helpers/rolesSelector';
+import { fixtureGenerateIdentitySetDataCorrectionCsvFile } from '../../../fixtures/mass-action-data-correction';
+import {
+  MassActionDataCorrectionFileParams,
+  createEventAndTeam,
+  getPersonsInfo,
+  prepareStateMassActionDataCorrectionFile,
+  prepareStateMultipleHouseholds,
+} from '../../helpers/prepareState';
+import { removeTeamMembersFromTeam } from '../../helpers/teams';
+import { processDataCorrectionFileSteps } from './canSteps';
+
+const canRoles = [
+  UserRoles.level6,
+];
+
+const cannotRoles = [
+  UserRoles.level5,
+  UserRoles.level4,
+  UserRoles.level3,
+  UserRoles.level2,
+  UserRoles.level1,
+  UserRoles.level0,
+  UserRoles.contributor1,
+  UserRoles.contributor2,
+  UserRoles.contributor3,
+  UserRoles.readonly,
+];
+
+const { filteredCanRoles, filteredCannotRoles, allRoles } = getRoles(canRoles, cannotRoles);
+
+let accessTokenL6 = '';
+const householdQuantity = 3;
+const filePath = 'cypress/downloads/identitySetDataCorrectionMassAction.csv';
+
+describe('#TC1837# - Process an Identity Set data correction file', { tags: ['@household', '@mass-actions'] }, () => {
+  describe('Can Roles', () => {
+    for (const roleName of filteredCanRoles) {
+      describe(`${roleName}`, () => {
+        beforeEach(() => {
+          cy.getToken().then(async (tokenResponse) => {
+            accessTokenL6 = tokenResponse.access_token;
+            const resultPrepareStateEvent = await createEventAndTeam(accessTokenL6, allRoles);
+            // eslint-disable-next-line
+            const resultCreatedMultipleHousehold = await prepareStateMultipleHouseholds(accessTokenL6, resultPrepareStateEvent.event, householdQuantity);
+            const householdMemberIds: string[] = [
+              resultCreatedMultipleHousehold.householdsCreated[0].registrationResponse.household.members[0],
+              resultCreatedMultipleHousehold.householdsCreated[1].registrationResponse.household.members[0],
+              resultCreatedMultipleHousehold.householdsCreated[2].registrationResponse.household.members[0],
+            ];
+            const resultPersonsInfo = await getPersonsInfo(resultCreatedMultipleHousehold.provider, householdMemberIds);
+            const memberHouseholds: Record<string, string> = {
+              [resultPersonsInfo[0].id]: resultPersonsInfo[0].etag,
+              [resultPersonsInfo[1].id]: resultPersonsInfo[1].etag,
+              [resultPersonsInfo[2].id]: resultPersonsInfo[2].etag,
+            };
+            const resultGenerateCsvFile = fixtureGenerateIdentitySetDataCorrectionCsvFile(memberHouseholds, filePath);
+
+            const massActionDataCorrectionFileParamData: MassActionDataCorrectionFileParams = {
+              provider: resultCreatedMultipleHousehold.provider,
+              dataCorrectionType: MassActionDataCorrectionType.IdentitySet,
+              generatedCsvFile: resultGenerateCsvFile,
+              correctionType: 'Identity Set',
+            };
+            const resultMassFinancialAssistance = await prepareStateMassActionDataCorrectionFile(massActionDataCorrectionFileParamData);
+            cy.wrap(resultPrepareStateEvent.provider).as('provider');
+            cy.wrap(resultPrepareStateEvent.team).as('teamCreated');
+            cy.login(roleName);
+            cy.goTo(`mass-actions/data-correction/details/${resultMassFinancialAssistance.id}`);
+          });
+        });
+        afterEach(function () {
+          if (this.teamCreated?.id && this.provider) {
+            removeTeamMembersFromTeam(this.teamCreated.id, this.provider);
+          }
+        });
+        it('should successfully process a Identity Set data correction file', () => {
+          processDataCorrectionFileSteps(householdQuantity, 'household records');
+        });
+      });
+    }
+  });
+  describe('Cannot Roles', () => {
+    for (const roleName of filteredCannotRoles) {
+      describe(`${roleName}`, () => {
+        beforeEach(() => {
+          cy.login(roleName);
+          cy.goTo('mass-actions/data-correction/create');
+        });
+        it('should not be able to process a Identity Set data correction file', () => {
+          cy.contains('You do not have permission to access this page').should('be.visible');
+        });
+      });
+    }
+  });
+});
