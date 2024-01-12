4451bda8df1dedfc08692001d9d8daf6fc41c543
Manmohan Singh Brar <manmohansingh.brar@redcross.ca>
2024-01-12 09:40:27 -0500

    comments resolution

--- apps/emis-app/cypress/pages/manage-duplicates/manageDuplicates.page.ts
+++ apps/emis-app/cypress/pages/manage-duplicates/manageDuplicates.page.ts
@@ -65,7 +65,7 @@ export class ManageDuplicatesPage {
 
   public getDuplicatePhoneNumber() {
     // eslint-disable-next-line
-    return cy.getByDataTest(this.duplicatePhoneNumber).getAndTrimText().then(phoneNumber => phoneNumber.replace(/[\(\)\-\s]/g, ''));
+    return cy.getByDataTest(this.duplicatePhoneNumber).getAndTrimText().then(phoneNumber => phoneNumber.replace(/[\(\)\-\s]/g, '')); //Modifies phoneNumber from bracketed type i.e. (646) - 767 8898 to unformatted type i.e. 6467678898
   }
 
   public getDuplicateName() {
