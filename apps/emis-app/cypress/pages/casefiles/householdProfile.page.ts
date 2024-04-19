0485f51d6a7791c32ad112a9391b700202694251
Léo Clipet <lclipet@redcross.ca>
2024-04-19 17:44:12 +0000

    Merged PR 12526: Cypress - Increase wait for activities endpoint
    Update householdProfile.page.ts

--- apps/emis-app/cypress/pages/casefiles/householdProfile.page.ts
+++ apps/emis-app/cypress/pages/casefiles/householdProfile.page.ts
@@ -139,7 +139,7 @@ export class HouseholdProfilePage {
 
   public goToCaseFileDetailsPage() {
     cy.getByDataTest(this.caseFileNumber).click();
-    cy.waitForStatusCode('**/case-file/case-files/*/activities', 200, 20000); // There are many calls done between the click on fetch of activities, ie all fetch from CaseFileDetails + all fetch from CaseFileActivity
+    cy.waitForStatusCode('**/case-file/case-files/*/activities', 200, 50000); // There are many calls done between the click on fetch of activities, ie all fetch from CaseFileDetails + all fetch from CaseFileActivity
     return new CaseFileDetailsPage();
   }
 
