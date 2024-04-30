3419808d4cebea942688ee8a6d0542c87d83816a
Haoyang Li <Hli@crc-tech.ca>
2024-04-30 20:24:10 +0000

    Merged PR 12629: [EMISV2-9222] TC:1967- Records can be processed Mass Action FA upload file when Authentication status check passed
    https://rctech.atlassian.net/browse/EMISV2-9222

--- apps/emis-app/cypress/e2e/critical-path-tests/critical-path-5/TC1967.RecordCanBeProcessedInMassActionFAUploadFileWhenAuthenticationStatusCheckPassed.cy.ts
+++ apps/emis-app/cypress/e2e/critical-path-tests/critical-path-5/TC1967.RecordCanBeProcessedInMassActionFAUploadFileWhenAuthenticationStatusCheckPassed.cy.ts
@@ -3,12 +3,12 @@ import { getRoles } from '@libs/cypress-lib/helpers/rolesSelector';
 import { EFinancialAmountModes } from '@libs/entities-lib/financial-assistance';
 import { IMassActionEntity } from '@libs/entities-lib/mass-action';
 import { IEligibilityCriteria, ProgramEntity } from '@libs/entities-lib/program';
+import { IdentityAuthenticationMethod, IdentityAuthenticationStatus, IIdentityAuthentication } from '@libs/entities-lib/case-file';
+import { identificationIdProvided } from '@libs/cypress-lib/helpers';
 import {
-  addAssessmentToCasefile, CasefileAssessmentParams, completeAndSubmitCasefileAssessmentByCrcUser,
-  createAndUpdateAssessment,
   createEventAndTeam,
   createProgramWithTableWithItemAndSubItem, MassActionFinancialAssistanceUploadFileParams,
-  prepareStateMassActionFinancialAssistanceUploadFile, updateProgram,
+  prepareStateMassActionFinancialAssistanceUploadFile, updateAuthenticationOfIdentity, updateProgram,
 } from '../../helpers/prepareState';
 import { removeTeamMembersFromTeam } from '../../helpers/teams';
 import { massActionFinancialAssistanceUploadFilePassesProcessCanSteps } from './canStep';
@@ -37,7 +37,7 @@ const householdQuantity = 1;
 let massFinancialAssistance = {} as IMassActionEntity;
 
 describe(
-  '#TC1965# - Record can be processed in Mass Action FA upload file when Assessment is mandatory and CRC User has completed the Assessment',
+  '#TC1967# - Record can be processed in Mass Action FA upload file when Authentication status check passed',
   { tags: ['@financial-assistance', '@mass-action'] },
   () => {
     before(() => {
@@ -75,33 +75,29 @@ describe(
               tableId: this.faTable.id,
               programId: this.program.id,
               householdQuantity,
-              filePath: 'cypress/downloads/TC1965FaFile.csv',
+              filePath: 'cypress/downloads/TC1967FaFile.csv',
             };
             const resultMassFinancialAssistance = await prepareStateMassActionFinancialAssistanceUploadFile(massActionFaUploadFileParamData);
             const { responseMassFinancialAssistance, responseCreateHouseholds } = resultMassFinancialAssistance;
             massFinancialAssistance = responseMassFinancialAssistance;
             const caseFileId = responseCreateHouseholds.householdsCreated[0].registrationResponse.caseFile.id;
-            const resultAssessment = await createAndUpdateAssessment(this.provider, this.event.id, this.program.id);
             const eligibilityCriteria: IEligibilityCriteria = {
-              authenticated: false,
+              authenticated: true,
               impacted: false,
-              completedAssessments: true,
-              completedAssessmentIds: [resultAssessment.id],
+              completedAssessments: false,
+              completedAssessmentIds: [],
             };
             const updateProgramEntity = new ProgramEntity({ ...this.program, eligibilityCriteria });
             await updateProgram(this.provider, updateProgramEntity);
-            const resultCreateAssessmentResponse = await addAssessmentToCasefile(
-              this.provider,
-              caseFileId,
-              resultAssessment.id,
-              );
-            const completeAndSubmitCasefileAssessmentParamData: CasefileAssessmentParams = {
-              provider: this.provider,
-              assessmentResponseId: resultCreateAssessmentResponse.id,
-              casefileId: caseFileId,
-              assessmentFormId: resultAssessment.id,
+            const params: IIdentityAuthentication = {
+              identificationIds: [{
+                optionItemId: identificationIdProvided.CanadianCitizenshipCard,
+                specifiedOther: null,
+              }],
+              method: IdentityAuthenticationMethod.InPerson,
+              status: IdentityAuthenticationStatus.Passed,
             };
-            await completeAndSubmitCasefileAssessmentByCrcUser(completeAndSubmitCasefileAssessmentParamData);
+            await updateAuthenticationOfIdentity(this.provider, caseFileId, params);
             cy.login(roleName);
             cy.goTo(`mass-actions/financial-assistance/details/${resultMassFinancialAssistance.responseMassFinancialAssistance.id}`);
           });
