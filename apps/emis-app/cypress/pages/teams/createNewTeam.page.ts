5928a76627852818cafd4eaae20e8b551679b544
Haoyang Li <hli@crc-tech.com>
2024-06-10 13:20:52 -0400

    update t29474

--- apps/emis-app/cypress/pages/teams/createNewTeam.page.ts
+++ apps/emis-app/cypress/pages/teams/createNewTeam.page.ts
@@ -3,20 +3,36 @@ export enum DataTest {
   primaryContact = 'team-contact',
   eventList = 'events',
   teamCreate = 'createEditTeam__submit',
+  cancelButton = 'createEditTeam__cancel',
   memberName = 'teamMembers__member_name',
   memberRole = 'teamMembers__member_role',
   memberTeamCount = 'teamMembers__member_teamCount',
   memberCaseFileCount = 'teamMembers__member_caseFileCount',
   memberOpenCaseFileCount = 'teamMembers__member_openCaseFileCount',
   memberInactiveCaseFileCount = 'teamMembers__member_inactiveCaseFileCount',
+  memberPhoneNumber = 'teamMembers__member_phoneNumber',
+  memberEmailAddress = 'teamMembers__member_email_address',
+  teamIsEscalationCheckbox = 'team-isEscalation-checkbox',
+  teamIsAssignableCheckbox = 'team-isAssignable-checkbox',
+  addNewMemberButton = 'add-new-member',
 }
 
 export class CreateNewTeamPage {
   private teamName = { selector: DataTest.teamName, type: 'input' };
 
+  private teamNameSpan = { selector: DataTest.teamName, type: 'span' };
+
+  private eventListDiv = { selector: DataTest.eventList, type: 'div' };
+
   private primaryContact = { selector: DataTest.primaryContact };
 
+  private primaryContactDiv = { selector: DataTest.primaryContact, type: 'div' };
+
   private teamCreate = { selector: DataTest.teamCreate };
+
+  private cancelButton = { selector: DataTest.cancelButton };
+
+  private addNewMemberButton = { selector: DataTest.addNewMemberButton };
 
   private memberName = { selector: DataTest.memberName };
 
@@ -30,6 +46,14 @@ export class CreateNewTeamPage {
 
   private memberInactiveCaseFileCount = { selector: DataTest.memberInactiveCaseFileCount };
 
+  private memberPhoneNumber = { selector: DataTest.memberPhoneNumber };
+
+  private memberEmailAddress = { selector: DataTest.memberEmailAddress };
+
+  private teamIsEscalationCheckbox = { selector: DataTest.teamIsEscalationCheckbox, type: 'input' };
+
+  private teamIsAssignableCheckbox = { selector: DataTest.teamIsAssignableCheckbox, type: 'input' };
+
   public fillTeamName(teamName: string) {
     cy.getByDataTest(this.teamName).type(teamName);
   }
@@ -41,6 +65,18 @@ export class CreateNewTeamPage {
   public fillPrimaryContactName(primaryContactName: string) {
     cy.getByDataTest(this.primaryContact).type(primaryContactName);
     cy.getByDataTest(this.primaryContact).selectListElementByValue(DataTest.primaryContact, primaryContactName);
+  }
+
+  public getTeamName() {
+    return cy.getByDataTest(this.teamNameSpan);
+  }
+
+  public getEventName() {
+    return cy.getByDataTest(this.eventListDiv);
+  }
+
+  public getPrimaryContact() {
+    return cy.getByDataTest(this.primaryContactDiv);
   }
 
   public createTeam() {
@@ -69,5 +105,33 @@ export class CreateNewTeamPage {
 
   public getMemberInactiveCaseFileCount() {
     return cy.getByDataTest(this.memberInactiveCaseFileCount);
+  }
+
+  public getMemberPhoneNumber() {
+    return cy.getByDataTest(this.memberPhoneNumber);
+  }
+
+  public getMemberEmailAddress() {
+    return cy.getByDataTest(this.memberEmailAddress);
+  }
+
+  public getTeamIsEscalationCheckbox() {
+    return cy.getByDataTest(this.teamIsEscalationCheckbox);
+  }
+
+  public getTeamIsAssignableCheckbox() {
+    return cy.getByDataTest(this.teamIsAssignableCheckbox);
+  }
+
+  public getTeamCreateButton() {
+    return cy.getByDataTest(this.teamCreate);
+  }
+
+  public getCancelButton() {
+    return cy.getByDataTest(this.cancelButton);
+  }
+
+  public getAddNewMemberButton() {
+    return cy.getByDataTest(this.addNewMemberButton);
   }
 }
--- apps/emis-app/cypress/pages/teams/teamsHome.page.ts
+++ apps/emis-app/cypress/pages/teams/teamsHome.page.ts
@@ -1,14 +1,21 @@
 import { TeamDetailsPage } from './teamDetails.page';
+import { CreateNewTeamPage } from './createNewTeam.page';
 
 export enum DataTest {
   createTeam = 'create-team-button',
   teamLink = 'team_link',
+  createStandardTeam = 'create-standard-team-link',
+  createAdHocTeam = 'create-adhoc-team-link',
 }
 
 export class TeamsHomePage {
-  private createTeam = { selector: DataTest.createTeam };
+  private createTeam = { selector: DataTest.createTeam, type: 'button' };
 
   private teamLink = { selector: DataTest.teamLink };
+
+  private createStandardTeam = { selector: DataTest.createStandardTeam };
+
+  private createAdHocTeam = { selector: DataTest.createAdHocTeam };
 
   public getCreateTeamButton() {
     return cy.getByDataTest(this.createTeam);
@@ -18,8 +25,21 @@ export class TeamsHomePage {
     return cy.getByDataTestLike(this.teamLink);
   }
 
+  public getCreateStandardTeam() {
+    return cy.getByDataTest(this.createStandardTeam);
+  }
+
+  public getCreateAdHocTeam() {
+    return cy.getByDataTest(this.createAdHocTeam);
+  }
+
   public goToFirstTeamDetails() {
     cy.getByDataTestLike(this.teamLink).eq(0).click();
     return new TeamDetailsPage();
+  }
+
+  public goToCreateAdHocTeamPage() {
+    this.getCreateAdHocTeam().click();
+    return new CreateNewTeamPage();
   }
 }
