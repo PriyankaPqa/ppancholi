51f642b318f40feb4a297c7e4230c8a60b0f9614
Haoyang Li <Hli@crc-tech.ca>
2024-06-10 20:59:54 +0000

    Merged PR 12951: [EMISV2-8980] T29474( TC:1926) - Create Escalation Team EMISV2-T29474
    https://rctech.atlassian.net/browse/EMISV2-8980

--- apps/emis-app/cypress/e2e/helpers/page.ts
+++ apps/emis-app/cypress/e2e/helpers/page.ts
@@ -1,6 +1,12 @@
 import { AddFinancialAssistancePage } from 'cypress/pages/financial-assistance-payment/addFinancialAssistance.page';
+import { CreateNewTeamPage } from '../../pages/teams/createNewTeam.page';
 
   export const verifyAndReturnAddFaPaymentPage = (): AddFinancialAssistancePage | null => {
     cy.getByDataTest({ selector: 'page-title', type: 'h1' }).contains('Add financial assistance').should('be.visible');
     return new AddFinancialAssistancePage();
   };
+
+export const verifyAndReturnCreateNewTeamPage = (): CreateNewTeamPage | null => {
+  cy.getByDataTest({ selector: 'page-title', type: 'h1' }).contains('Create new team').should('be.visible');
+  return new CreateNewTeamPage();
+};
--- apps/emis-app/cypress/pages/teams/teamsHome.page.ts
+++ apps/emis-app/cypress/pages/teams/teamsHome.page.ts
@@ -3,12 +3,18 @@ import { TeamDetailsPage } from './teamDetails.page';
 export enum DataTest {
   createTeam = 'create-team-button',
   teamLink = 'team_link',
+  createStandardTeam = 'create-standard-team-link',
+  createAdHocTeam = 'create-adhoc-team-link',
 }
 
 export class TeamsHomePage {
-  private createTeam = { selector: DataTest.createTeam };
+  private createTeam = { selector: DataTest.createTeam, type: 'button' };
 
   private teamLink = { selector: DataTest.teamLink };
+
+  private createStandardTeam = { selector: DataTest.createStandardTeam };
+
+  private createAdHocTeam = { selector: DataTest.createAdHocTeam };
 
   public getCreateTeamButton() {
     return cy.getByDataTest(this.createTeam);
@@ -18,8 +24,20 @@ export class TeamsHomePage {
     return cy.getByDataTestLike(this.teamLink);
   }
 
+  public getCreateStandardTeam() {
+    return cy.getByDataTest(this.createStandardTeam);
+  }
+
+  public getCreateAdHocTeam() {
+    return cy.getByDataTest(this.createAdHocTeam);
+  }
+
   public goToFirstTeamDetails() {
     cy.getByDataTestLike(this.teamLink).eq(0).click();
     return new TeamDetailsPage();
+  }
+
+  public goToCreateAdHocTeamPage() {
+    this.getCreateAdHocTeam().click();
   }
 }
