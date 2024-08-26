ded44204bfbe4a09fe9fd248d317acd7ba176bef
Dana Nica <dnica@redcross.ca>
2024-08-26 20:07:50 +0000

    Merged PR 13362: [EMIS-9800] BE/FE Appointment booking - View appt program details - view service options
    https://rctech.atlassian.net/browse/EMISV2-9800

--- apps/emis-app/src/ui/views/pages/appointment-programs/components/ServiceOptionsTable.vue
+++ apps/emis-app/src/ui/views/pages/appointment-programs/components/ServiceOptionsTable.vue
@@ -71,6 +71,7 @@ import { useAppointmentProgramStore } from '@/pinia/appointment-program/appointm
 import { IOptionItem } from '@libs/entities-lib/optionItem';
 import { IListOption } from '@libs/shared-lib/types';
 import StatusChip from '@/ui/shared-components/StatusChip.vue';
+import routes from '@/constants/routes';
 import CreateEditServiceOption from '../create-edit/CreateEditServiceOption.vue';
 
 export interface IExtendedServiceOption extends IServiceOption {
@@ -185,7 +186,7 @@ export default Vue.extend({
   methods: {
     getDetailsRoute(id: string) {
       return {
-        name: '',
+        name: routes.events.appointmentPrograms.serviceOptionDetails.name,
         params: {
            appointmentProgramId: this.appointmentProgramId,
            serviceOptionId: id,
@@ -206,14 +207,25 @@ export default Vue.extend({
       this.showServiceOptionDialog = true;
     },
 
-    // TODO - finalize in next story (#9800), this is a draft
-    deleteServiceOption(serviceOption: IExtendedServiceOption) {
+    async deleteServiceOption(serviceOption: IExtendedServiceOption) {
       if (this.isEditMode) {
-        const res = useAppointmentProgramStore().deleteServiceOption(this.appointmentProgramId, serviceOption.id);
-        if (res) {
-          this.$toasted.global.success(this.$t('appointmentProgram.serviceOption.delete.success'));
-        } else {
-          this.$toasted.global.error(this.$t('appointmentProgram.serviceOption.delete.error'));
+        if (this.serviceOptions.length === 1) {
+          this.$message({ title: this.$t('common.error'), message: this.$t('appointmentProgram.serviceOption.deleteUniqueServiceOption.error') });
+          return;
+        }
+
+        const userChoice = await this.$confirm({
+          title: this.$t('appointmentProgram.serviceOption.confirm.delete.title'),
+          messages: this.$t('appointmentProgram.serviceOption.confirm.delete.message'),
+         });
+
+        if (userChoice) {
+          const res = await useAppointmentProgramStore().deleteServiceOption(this.appointmentProgramId, serviceOption.id);
+          if (res) {
+            this.$toasted.global.success(this.$t('appointmentProgram.serviceOption.delete.success'));
+          } else {
+            this.$toasted.global.error(this.$t('appointmentProgram.serviceOption.delete.error'));
+          }
         }
       } else {
         const index = this.serviceOptions.findIndex((o) => o.tempId === serviceOption.tempId);
@@ -264,6 +276,11 @@ export default Vue.extend({
 
 .no-bottom-border {
   border-bottom: none;
+}
+
+::v-deep .v-data-table > .v-data-table__wrapper tbody tr td:has(> button){
+  padding-left: 0;
+  padding-right: 0;
 }
 
 </style>
---
+++ apps/emis-app/src/ui/views/pages/appointment-programs/details/ServiceOptionDetails.spec.js
@@ -0,0 +1,208 @@
+import { createLocalVue, shallowMount, mount } from '@/test/testSetup';
+import { useMockAppointmentProgramStore } from '@/pinia/appointment-program/appointment-program.mock';
+import { mockAppointmentProgram, mockServiceOption } from '@libs/entities-lib/appointment';
+import { mockOptionItem } from '@libs/entities-lib/optionItem';
+import StatusChip from '@/ui/shared-components/StatusChip.vue';
+import Component from './ServiceOptionDetails.vue';
+
+const localVue = createLocalVue();
+const { pinia, appointmentProgramStore } = useMockAppointmentProgramStore();
+
+describe('ServiceOptionDetails.vue', () => {
+  let wrapper;
+
+  const mountWrapper = async (shallow = true, otherOptions = {}) => {
+    jest.clearAllMocks();
+    wrapper = (shallow ? shallowMount : mount)(Component, {
+      localVue,
+      pinia,
+      propsData: {
+        show: true,
+        appointmentProgramId: 'appt-program-id',
+        serviceOptionId: 'so-id',
+      },
+      ...otherOptions,
+    });
+  };
+
+  describe('Computed', () => {
+    describe('serviceOptionTypeName', () => {
+      it('returns the right type name', () => {
+        appointmentProgramStore.getServiceOptionTypes = jest.fn(() => [mockOptionItem({ id: 'oi-1', name: { translation: { en: 'name-oi-1' } } })]);
+        mountWrapper(true, { computed: { serviceOption() {
+          return mockServiceOption({ serviceOptionType: { optionItemId: 'oi-1' } });
+        } } });
+        expect(wrapper.vm.serviceOptionTypeName).toEqual({ translation: { en: 'name-oi-1' } });
+      });
+    });
+
+    describe('appointmentModalities', () => {
+      it('returns the  modalities of the service option as option items', () => {
+        const modality = mockOptionItem({ id: 'oi-1', name: { translation: { en: 'name-oi-1' } } });
+        appointmentProgramStore.getAppointmentModalities = jest.fn(() => [modality]);
+        mountWrapper(true, { computed: { serviceOption() {
+          return mockServiceOption({ appointmentModalities: [{ optionItemId: modality.id }] });
+        } } });
+        expect(wrapper.vm.appointmentModalities).toEqual([modality]);
+      });
+    });
+
+    describe('serviceOption', () => {
+      it('returns the service options of the appt program', async () => {
+        const so = mockServiceOption({ id: 'so-id' });
+        appointmentProgramStore.fetch = jest.fn(() => mockAppointmentProgram({ serviceOptions: [so] }));
+        await mountWrapper();
+        await wrapper.setData({ appointmentProgram: mockAppointmentProgram({ serviceOptions: [so] }) });
+        expect(wrapper.vm.serviceOption).toEqual(so);
+      });
+    });
+
+    describe('existingTypesIds', () => {
+      it('returns the types ids without the id of the current service option type', async () => {
+        const so1 = mockServiceOption({ id: 'so-id', serviceOptionType: { optionItemId: 'oi-1' } });
+        const so2 = mockServiceOption({ serviceOptionType: { optionItemId: 'oi-2' } });
+        const so3 = mockServiceOption({ serviceOptionType: { optionItemId: 'oi-3' } });
+        appointmentProgramStore.fetch = jest.fn(() => mockAppointmentProgram({ serviceOptions: [so1, so2, so3] }));
+        await mountWrapper(true, { computed: { serviceOption() {
+          return mockServiceOption({ serviceOptionType: { optionItemId: 'oi-1' } });
+        } } });
+        await wrapper.setData({ appointmentProgram: mockAppointmentProgram({ serviceOptions: [so1, so2, so3] }) });
+        expect(wrapper.vm.existingTypesIds).toEqual(['oi-2', 'oi-3']);
+      });
+    });
+  });
+
+  describe('Lifecycle', () => {
+    describe('created', () => {
+      it('should call store fetchServiceOptionTypes and fetchAppointmentModalitiesand fetch by id', async () => {
+        mountWrapper();
+
+        await wrapper.vm.$options.created.forEach((hook) => {
+          hook.call(wrapper.vm);
+        });
+        await wrapper.vm.$nextTick();
+        expect(appointmentProgramStore.fetchServiceOptionTypes).toHaveBeenCalled();
+        expect(appointmentProgramStore.fetchAppointmentModalities).toHaveBeenCalled();
+        expect(appointmentProgramStore.fetch).toHaveBeenCalledWith(wrapper.vm.appointmentProgramId);
+      });
+    });
+  });
+
+  describe('Methods', () => {
+    describe('navigateBack', () => {
+      it('calls router back', async () => {
+        await mountWrapper(false);
+        wrapper.vm.$router.back = jest.fn();
+        wrapper.vm.navigateBack();
+        expect(wrapper.vm.$router.back).toHaveBeenCalled();
+      });
+    });
+
+    describe('deleteServiceOption', () => {
+      it(' shows an error when deleting the last service option', async () => {
+        const so = mockServiceOption({ id: '2' });
+        appointmentProgramStore.fetch = jest.fn(() => mockAppointmentProgram({ serviceOptions: [so] }));
+        await mountWrapper(true, { computed: { serviceOption() {
+          return so;
+        } } });
+        await wrapper.setData({ appointmentProgram: mockAppointmentProgram({ serviceOptions: [so] }) });
+        wrapper.vm.deleteServiceOption();
+        expect(wrapper.vm.$message).toHaveBeenCalledWith({ title: 'common.error', message: 'appointmentProgram.serviceOption.deleteUniqueServiceOption.error' });
+      });
+
+      it('calls confirm and calls store delete and a success message is displayed', async () => {
+        const so2 = mockServiceOption({ id: '2' });
+        await mountWrapper(true, { computed: { serviceOption() {
+          return so2;
+        } } });
+        wrapper.vm.$confirm = jest.fn(() => true);
+        await wrapper.vm.deleteServiceOption();
+        expect(wrapper.vm.$confirm).toHaveBeenCalledWith({ title: 'appointmentProgram.serviceOption.confirm.delete.title',
+          messages: 'appointmentProgram.serviceOption.confirm.delete.message' });
+        expect(appointmentProgramStore.deleteServiceOption).toHaveBeenCalledWith(wrapper.vm.appointmentProgramId, so2.id);
+        expect(wrapper.vm.$toasted.global.success).toHaveBeenCalledWith('appointmentProgram.serviceOption.delete.success');
+      });
+
+      it('displays an error message on store call error', async () => {
+        const so2 = mockServiceOption({ id: '2' });
+        await mountWrapper(true, { computed: { serviceOption() {
+          return so2;
+        } } });
+        wrapper.vm.$confirm = jest.fn(() => true);
+        appointmentProgramStore.deleteServiceOption = jest.fn();
+        await wrapper.vm.deleteServiceOption();
+        expect(wrapper.vm.$toasted.global.error).toHaveBeenCalledWith('appointmentProgram.serviceOption.delete.error');
+      });
+    });
+  });
+
+  describe('template', () => {
+    describe('type name', () => {
+      it('contains the name of the service option type', async () => {
+        await mountWrapper(true, { computed: { serviceOptionTypeName() {
+          return { translation: { en: 'type-name' } };
+        },
+        serviceOption() {
+          return mockServiceOption();
+        } } });
+        await wrapper.setData({ loading: false });
+        const element = wrapper.findDataTest('serviceOption_details_type');
+        expect(element.exists()).toBeTruthy();
+        expect(element.text()).toContain('type-name');
+      });
+    });
+
+    describe('status', () => {
+      it('renders', async () => {
+        await mountWrapper(true, { computed: { serviceOption() {
+          return mockServiceOption();
+        } } });
+        await wrapper.setData({ loading: false });
+        const statusChip = wrapper.findComponent(StatusChip);
+        expect(statusChip.exists()).toBeTruthy();
+        expect(statusChip.attributes('status')).toEqual('1');
+      });
+    });
+
+    describe('modalities', () => {
+      it('displayes all modalities names', async () => {
+        await mountWrapper(true, { computed: { appointmentModalities() {
+          return [
+            mockOptionItem({ id: 'mod-1', name: { translation: { en: 'mod-1-name' } } }),
+            mockOptionItem({ id: 'mod-2', name: { translation: { en: 'mod-2-name' } } }),
+          ];
+        },
+        serviceOption() {
+          return mockServiceOption();
+        } } });
+        await wrapper.setData({ loading: false });
+
+        const element = wrapper.findDataTest('serviceOption_modalities_name_mod-1');
+        expect(element.exists()).toBeTruthy();
+        expect(element.text()).toContain('mod-1-name');
+        const element2 = wrapper.findDataTest('serviceOption_modalities_name_mod-2');
+        expect(element2.exists()).toBeTruthy();
+        expect(element2.text()).toContain('mod-2-name');
+      });
+
+      it('displays which modalities are online', async () => {
+        await mountWrapper(true, { computed: { appointmentModalities() {
+          return [
+            mockOptionItem({ id: 'mod-1', name: { translation: { en: 'mod-1-name' } }, isOnline: false }),
+            mockOptionItem({ id: 'mod-2', name: { translation: { en: 'mod-2-name' } }, isOnline: true }),
+          ];
+        },
+        serviceOption() {
+          return mockServiceOption();
+        } } });
+        await wrapper.setData({ loading: false });
+        const element = wrapper.findDataTest('serviceOption_modalities_isOnline_mod-1');
+        expect(element.exists()).toBeTruthy();
+        expect(element.text()).not.toContain('appointmentProgram.serviceOption.details.isOnline');
+        const element2 = wrapper.findDataTest('serviceOption_modalities_isOnline_mod-2');
+        expect(element2.exists()).toBeTruthy();
+        expect(element2.text()).toContain('appointmentProgram.serviceOption.details.isOnline');
+      });
+    });
+  });
+});
