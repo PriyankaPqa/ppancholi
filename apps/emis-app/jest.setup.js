ff72a66c1e34fbb8da1a231b7e79f292e1d730c5
Leo Clipet <leo.clipet@redcross.ca>
2024-08-29 23:48:08 +0200

    Update jest.setup.js

--- apps/emis-app/jest.setup.js
+++ apps/emis-app/jest.setup.js
@@ -8,12 +8,12 @@ process.on('unhandledRejection', (reason, promise) => {
   throw reason;
 });
 
-// eslint-disable-next-line import/no-extraneous-dependencies
-const whyIsNodeRunning = require('why-is-node-running');
+// // eslint-disable-next-line import/no-extraneous-dependencies
+// const whyIsNodeRunning = require('why-is-node-running');
 
-afterAll(() => {
-  // Give Jest some time to close everything before running the check
-  setTimeout(() => {
-    whyIsNodeRunning(); // This will log what's keeping Node.js running
-  }, 1000);
-});
+// afterAll(() => {
+//   // Give Jest some time to close everything before running the check
+//   setTimeout(() => {
+//     whyIsNodeRunning(); // This will log what's keeping Node.js running
+//   }, 1000);
+// });
