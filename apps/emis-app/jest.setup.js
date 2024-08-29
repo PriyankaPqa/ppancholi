e5859933642978be4cf545e3ccb2d544f1a3716b
Leo Clipet <leo.clipet@redcross.ca>
2024-08-29 23:15:10 +0200

    test

--- apps/emis-app/jest.setup.js
+++ apps/emis-app/jest.setup.js
@@ -7,3 +7,13 @@ process.on('unhandledRejection', (reason, promise) => {
   console.error('Unhandled Rejection at:', promise, 'reason:', reason);
   throw reason;
 });
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+const whyIsNodeRunning = require('why-is-node-running');
+
+afterAll(() => {
+  // Give Jest some time to close everything before running the check
+  setTimeout(() => {
+    whyIsNodeRunning(); // This will log what's keeping Node.js running
+  }, 1000);
+});
--- jest.config.base.js
+++ jest.config.base.js
@@ -18,7 +18,7 @@ module.exports = {
     '.+\\.(css|styl|less|sass|scss|jpg|jpeg|png|svg|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|avif)$': 'jest-transform-stub',
     '.*\\.(vue)$': '@vue/vue2-jest',
     '^.+\\.(ts|tsx)$': 'ts-jest',
-    '^.+\\.jsx?$': 'babel-jest'
+    '^.+\\.jsx?$': 'babel-jest',
   },
   testMatch: [
     '**/*.(spec|test).(js|jsx|ts|tsx)',
