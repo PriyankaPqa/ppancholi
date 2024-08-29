2a3b68a4d521d3e1f0bb4c683eeb85a8633aae1f
Leo Clipet <leo.clipet@redcross.ca>
2024-08-29 23:12:20 +0200

    Update jest.setup.js

--- apps/emis-app/jest.setup.js
+++ apps/emis-app/jest.setup.js
@@ -1,3 +1,9 @@
 import 'regenerator-runtime/runtime';
 
 jest.mock('@/auth/AuthenticationProvider');
+
+process.on('unhandledRejection', (reason, promise) => {
+  // eslint-disable-next-line no-console
+  console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+  throw reason;
+});
